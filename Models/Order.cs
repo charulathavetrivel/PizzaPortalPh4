9c4f6aa1be349e860d1b32606cdf3f1956870105
Charulatha <charulatha200701@gmail.com>
2024-02-25 12:32:27 +0530

    first commit

---
+++ Models/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace PizzaPortalPh4.Models
+{
+    public class Order
+    {
+        public string? Pizza { get; set; }
+        public int Quantity { get; set; }
+        public double Amount { get; set; }
+    }
+}
---
+++ Models/Pizza.cs
@@ -0,0 +1,9 @@
+﻿namespace PizzaPortalPh4.Models
+{
+    public class Pizza
+    {
+        public int PizzaId { get; set; }
+        public string? PizzaType { get; set; }
+        public double Price { get; set; }
+    }
+}
